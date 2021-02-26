4738116a1b76e675fcddc97ee3005943e8eff63f
hollymillea <58816730+hollymillea@users.noreply.github.com>
2021-02-26 09:33:49 +0000

    Add files via upload

---
+++ Week1/sheet1.py
@@ -0,0 +1,23 @@
+import random
+
+def ListToString():
+    return
+
+
+def GetEmptyCells():
+    return
+
+
+def GetRandomElement():
+    return
+
+
+def MaxIndices():
+    return
+
+# TESTING
+#print( ListToString(['A','B','C']) == 'ABC' )
+#print( GetEmptyCells([' ','B','C',' ']) == [0,3] ) 
+#print( GetRandomElement([1,2,3,4,5]) )
+#print( MaxIndices([0,8,20,5]) == [2] )
+#print( MaxIndices([7,2,5,7,5]) == [0,3] )
