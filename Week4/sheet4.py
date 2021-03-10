9b92898fc163fe237a7bb4f65e629f53c0294b75
hollymillea <58816730+hollymillea@users.noreply.github.com>
2021-03-10 17:13:28 +0000

    Add files via upload

---
+++ Week4/sheet4.py
@@ -0,0 +1,24 @@
+# TASK 2:
+def BinaryToInt(B):
+    return
+
+
+# TASK 3:
+def IntToBinary(N):
+    return
+
+
+# TASK 4:
+def IntToByte():
+    return
+
+def StringToBytes(String):
+    return
+
+def BytesToString(Bytes):
+    return
+
+
+# TASK 5:
+def UTFDecoder(BitStream):
+    return
